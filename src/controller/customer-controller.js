d3c221ff312c0cef9ee64deb1bb0a54bbb788f9c
a-dridi <admin@ard-site.net>
2021-11-28 23:47:11 +0100

    Version 1.0.3 Alpha

--- src/controller/customer-controller.js
+++ src/controller/customer-controller.js
@@ -84,38 +84,43 @@ exports.customerSave = function (req, res, next) {
  * Update customer with the data passed in the POST request.
  */
 exports.customerUpdate = function (req, res, next) {
-    let updateCustomer = new Customer({
-        _id: req.body._id,
-        companyName: req.body.companyName,
-        forename: req.body.forename,
-        surname: req.body.surname,
-        email: req.body.email,
-        telephone: req.body.telephone,
-        street: req.body.street,
-        city: req.body.city,
-        postCode: req.body.postCode,
-        country: req.body.country,
-        IBAN: req.body.IBAN,
-        BIC: req.body.BIC,
-        bankInformation: req.body.bankInformation,
-        website: req.body.website,
-        facebookUrl: req.body.facebookUrl,
-        twitterUrl: req.body.twitterUrl,
-        xingUrl: req.body.xingUrl,
-        socialmediaUrl: req.body.socialmediaUrl,
-        language: req.body.language,
-        timezone: req.body.timezone,
-        note: req.body.note,
-        deleted: false
+    console.log("customerUpdate!!!!!!!!")
+    console.log(req.body._id)
+    Customer.findById(req.body._id, (err, customer) => {
+        if (!customer) {
+            ApiResponse.errorResponse(res, "Could not load customer from database to perform update!");
+        } else {
+            customer.companyName = req.body.companyName;
+            customer.forename = req.body.forename;
+            customer.surname = req.body.surname;
+            customer.email = req.body.email;
+            customer.telephone = req.body.telephone;
+            customer.street = req.body.street;
+            customer.city = req.body.city;
+            customer.postCode = req.body.postCode;
+            customer.country = req.body.country;
+            customer.IBAN = req.body.IBAN;
+            customer.BIC = req.body.BIC;
+            customer.bankInformation = req.body.bankInformation;
+            customer.website = req.body.website;
+            customer.facebookUrl = req.body.facebookUrl;
+            customer.twitterUrl = req.body.twitterUrl;
+            customer.xingUrl = req.body.xingUrl;
+            customer.socialmediaUrl = req.body.socialmediaUrl;
+            customer.language = req.body.language;
+            customer.timezone = req.body.timezone;
+            customer.note = req.body.note;
+        }
+
+        customer.save()
+            .then(updatedCustomer => {
+                ApiResponse.successResponse(res, updatedCustomer);
+            })
+            .catch(err => {
+                ApiResponse.errorResponse(res, err);
+            });
     });
 
-    updateCustomer.save()
-        .then(updatedCustomer => {
-            ApiResponse.successResponse(res, updatedCustomer);
-        })
-        .catch(err => {
-            ApiResponse.errorResponse(res, err);
-        });
 };
 
 /**
